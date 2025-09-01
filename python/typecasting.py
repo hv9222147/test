1d426c2a8e0b635637286b0c8e52b8e43423a4bf
harsh <hv9222147@gmail.com>
2025-09-01 15:31:15 +0530

    add

---
+++ python/typecasting.py
@@ -0,0 +1,27 @@
+#colour = input("what is the colour of rose ")
+#print (colour)
+
+#type casting ti int  :
+
+#n = int (input("how many student in class:"))
+#print(n)
+
+#type casting to float :
+#price = float(input("price of the  roses :"))
+#print(price)
+
+
+#type of the data type  :
+a =10
+b=13.89
+c="harsh"
+d=("geeks for geeks")
+e=("geeks for geeks")
+f={"geeks": 1,"for": 2,"geeks": 3}
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
