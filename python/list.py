e50f1ce5e6badefa27f760fa70b6de8f954a98d9
harsh <hv9222147@gmail.com>
2025-09-04 15:37:31 +0530

    add

---
+++ python/list.py
@@ -0,0 +1,27 @@
+#list question :
+
+
+
+# movies=[]
+# mov1 = input(" enter your 1st movie")
+# movies.append(mov1)
+# mov2 = input("enter your 2nd movies")
+# movies.append(mov2)
+# mov3 = input("enter your 3rd movies")
+# movies.append(mov3)
+
+
+# print(movies)
+
+# list palridrome question :
+
+list1 = [1,2,1]
+
+
+copy_list1 = list1.copy()
+copy_list1.reverse()
+
+if(copy_list1 ==list1):
+    print("IT IS A PALIDROME")
+else:
+    print("IT IS NOT A PALIDROME")
