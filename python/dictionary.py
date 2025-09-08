fcde2d44fd88dc8ce85e6a57f0a044c85217d4f5
harsh <hv9222147@gmail.com>
2025-09-08 17:35:34 +0530

    add

--- python/dictionary.py
+++ python/dictionary.py
@@ -1,9 +1,22 @@
-dictionary = {
-    "cat" : "a small animal",
-    "table" : [" a piece of furniture", "list of fact and figure"]
+# dictionary = {
+#     "cat" : "a small animal",
+#     "table" : [" a piece of furniture", "list of fact and figure"]
 
-}
+# }
 
 
-print(dictionary)
+# print(dictionary)
 
+# i = 1
+# while i <=10:
+#     print(i)
+#     i += 1
+
+
+num = (1,4,9,16,25,36,49,64,81,100)
+
+x = 36
+
+while i<len(num):
+    print(num(1))
+    i +=1
