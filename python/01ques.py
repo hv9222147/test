59bff0c0faf2a8cb66d43c5f1c873ebaf4863bce
harsh <hv9222147@gmail.com>
2025-09-23 23:39:21 +0530

    add all

---
+++ python/01ques.py
@@ -0,0 +1,45 @@
+# question :
+
+
+# def loop(num):
+#     for i in range(1,num+1):
+#         print(i)
+
+# num = int(input("enter a number: "))
+# result = loop(num)
+# print("the number is: ",result)
+
+
+# question 2:
+
+# def is_positive(num):
+#     if num > 0:
+#         return True
+#     else:
+#         return False
+# num = int(input("enter a number: "))
+# result = is_positive(num)
+# print("the number  is positive:  ",result)
+
+
+# question 3:
+
+# def max_of_two(a,b):
+#     if a>b:
+#         return a
+#     else:
+#         return b
+
+# a = int(input("enter  first number:"))
+# b = int(input("enter  second number:"))
+# result = max_of_two(a,b)
+# print("the number is max:",result)
+
+
+# question 4:
+def loop(n):
+    for i in range(1,n+1):
+        print(i)
+n = int(input("enter a number: "))
+result = loop(n)
+print("the number is: ",result)
