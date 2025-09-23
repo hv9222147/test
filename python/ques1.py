db4b0149c8ccbbab6a6c812d6b00d6519466998a
harsh <hv9222147@gmail.com>
2025-09-23 15:15:55 +0530

    add

--- python/ques1.py
+++ python/ques1.py
@@ -15,6 +15,28 @@
 #     print("You lose!")
 
 
-x = 7
-y = 2
-z = x>3 and y<1
+# x = 7
+# y = 2
+# z = x>3 and y<1
+
+
+
+#question1 for python in function:
+
+# def sum_all(*args):
+#     print(args)
+#     for i in args:
+#         print(i * 2)
+#         return sum(args)
+# print(sum_all(1,2,3))
+# print(sum_all(1,2,3,4,5,6,7))
+
+
+#question 2 for python in function:
+
+def kwargs(**kwargs):
+    print(kwargs)
+    for key,value in kwargs.items():
+     print(f"{key} : {value}")
+    return kwargs
+print(kwargs(name ="harsh verma",age=21,city="Bhiwani"))
